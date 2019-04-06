bbb87b021e8300ca3b20ff5b1aad6785f40deda0
Anastassiya Duisembayeva <imperator-taya@mail.ru>
2019-04-06 22:39:33 +0600

    Creation Fisrt Activity for list 1

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.homework2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+ //   @Override
+ //   public boolean onOptionsItemSelected(MenuItem item) {
+  //      int id = item.getItemId();
+
+   //     switch(id){
+     //       case R.id.action_settings :
+
+       //         return true;
+       //     case R.id.open_settings:
+
+       //         return true;
+       //     case R.id.save_settings:
+
+       //         return true;
+       // }
+      //  return super.onOptionsItemSelected(item);
+    //}
 }

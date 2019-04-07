f0df5031c306ca665800768feb0e68a9bc1d4fd3
Anastassiya Duisembayeva <imperator-taya@mail.ru>
2019-04-07 17:09:54 +0600

    Settings items

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homework2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -20,21 +21,24 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
- //   @Override
- //   public boolean onOptionsItemSelected(MenuItem item) {
-  //      int id = item.getItemId();
+    @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-   //     switch(id){
-     //       case R.id.action_settings :
-
-       //         return true;
-       //     case R.id.open_settings:
-
-       //         return true;
-       //     case R.id.save_settings:
-
-       //         return true;
-       // }
-      //  return super.onOptionsItemSelected(item);
-    //}
+        switch(id){
+            case R.id.item1 :
+                Intent intent1 = new Intent(this, Item1Activity.class);
+                startActivity(intent1);
+                return true;
+           case R.id.item2:
+               Intent intent2 = new Intent(this, Item2Activity.class);
+               startActivity(intent2);
+                return true;
+           case R.id.item3:
+               Intent intent3 = new Intent(this, Item3Activity.class);
+               startActivity(intent3);
+               return true;
+       }
+        return super.onOptionsItemSelected(item);
+    }
 }
